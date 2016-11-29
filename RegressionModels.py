5f530b5d96bd586a738f3b7f0ee0028f8875836b
Milzi <mastermilz@web.de>
2016-11-29 16:57:33 +0100

    Update RegressionModels.py

--- RegressionModels.py
+++ RegressionModels.py
@@ -7,7 +7,10 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
 
-def trainModel(inputData, labels):
+def trainCartModel(inputData, labels):
     
     regressor = DecisionTreeRegressor(random_state=0)
-    cross_val_score(regressor, inputData, labels, cv=10)
+    score = cross_val_score(regressor, inputData, labels, cv=2)
+    
+    print("CART - Regression Model trained with score: ")
+    print (score)
