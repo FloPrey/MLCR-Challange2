86fdf1ef8f52798ab44078ede7af76d5d88fa8a2
Milzi <mastermilz@web.de>
2016-12-02 18:26:17 +0100

    split labels hinzugefügt nur für mein model wichtig

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
-'''
-Created on Nov 28, 2016
-
-@author: tobi
-'''
 import Challenge2.DatasetCreator as dc
 import Challenge2.RegressionModels as models
 
 dataset = dc.createDataSet()
 
-trainF_x, trainF_y, testF_x, testF_y = dc.inputOutputDataFreeDays(dataset, False)
-trainW_x, trainW_y, testW_x, testW_y = dc.inputOutputDataWorkDays(dataset, False)
+datasetFree = dc.inputOutputDataFreeDays(dataset, False)
+datasetWork = dc.inputOutputDataWorkDays(dataset, False)
+
+# split dataset into input (x-value) and output (y-value)
+inputDataFree, outputDataFree = dc.splitLabels(datasetFree)
+inputDataWork, outputDataWork = dc.splitLabels(datasetWork)
+
+# split entire dataset into train and test sets and their corresponding outputl values
+trainF_x, trainF_y, testF_x, testF_y = dc.splitDataset(datasetFree)
+trainW_x, trainW_y, testW_x, testW_y = dc.splitDataset(datasetWork)
 
 print("Train Set free")
 print (trainF_x)
 print (trainF_y)
-
 print("--------------------")
 print("Test Set free")
 print (testF_x)
@@ -28,7 +30,8 @@ print("--------------------")
 print (testW_x)
 print (testW_y)
 
-#do regression
-#models.trainCartModel(inputWorkDays, outputWorkDays)
-#models.trainAdaBoostModel(inputWorkDays, outputWorkDays)
+# use entire dataset for training with cross-fold-validation
+#models.adaBoostModel(inputDataWork, outputDataWork)
+
+# use seperate train and test set
 #models.trainSVR(inputWorkDays, outputWorkDays, kernel = 'rbf')
