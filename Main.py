24d16d171852616d7c5a7cd18c42e5f8beb0b2b9
Milzi <mastermilz@web.de>
2016-12-04 16:13:07 +0100

    bitte euren linear regression models aufruf einfüg

--- Main.py
+++ Main.py
@@ -10,33 +10,18 @@ datasetWork = dc.inputOutputDataWorkDays(dataset)
 inputDataFree, outputDataFree = dc.splitLabels(datasetFree)
 inputDataWork, outputDataWork = dc.splitLabels(datasetWork)
 
-# split entire dataset into train and test sets and their corresponding outputl values
+# split entire dataset into train and test sets and their corresponding output values
+# horizontall split for every participant
 trainF_x, trainF_y, testF_x, testF_y = dc.splitDataset(datasetFree)
 trainW_x, trainW_y, testW_x, testW_y = dc.splitDataset(datasetWork)
 
-splitTrainW_x, splitTrainW_y, splitTestW_x, splitTextW_y = dc.splitByParticipant(trainW_x, trainW_y, testW_x, testW_y)
-print (trainW_x)
-print (splitTrainW_x[0])
-print (splitTrainW_x[5])
-
-print("Train Set free")
-print (trainF_x)
-print (trainF_y)
-print("--------------------")
-print("Test Set free")
-print (testF_x)
-print (testF_y)
-print("--------------------")
-print("Train set Work")
-print (trainW_x)
-print (trainW_y)
-print("Test set Work")
-print("--------------------")
-print (testW_x)
-print (testW_y)
-
 # use entire dataset for training with cross-fold-validation
-#models.adaBoostModel(inputDataWork, outputDataWork)
+models.adaBoostModelWithCrossFoldValidation(inputDataWork, outputDataWork, "Workdays")
+models.adaBoostModelWithCrossFoldValidation(inputDataFree, outputDataFree, "Freedays")
 
-# use seperate train and test set
-#models.trainSVR(inputWorkDays, outputWorkDays, kernel = 'rbf')
+# use already split dataset 
+models.adaBoostModel(trainW_x, trainW_y, testW_x, testW_y, "Workdays")
+models.adaBoostModel(trainF_x, trainF_y, testF_x, testF_y, "Freedays")
+
+splitTrainW_x, splitTrainW_y, splitTestW_x, splitTextW_y = dc.splitByParticipant(trainW_x, trainW_y, testW_x, testW_y)
+
